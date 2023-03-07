08c76f6098c1c26fc4bb9c449c73fba5899e1660
Pavel Maevsky <paha412011@mail.ru>
2023-03-08 00:16:04 +0300

    задача 8  закомечена

--- Program.cs
+++ Program.cs
@@ -73,7 +73,7 @@ else
 
 /*
 Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-*/
+
 Console.Write("Input pozitive number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int current = 2;
@@ -83,3 +83,4 @@ while (current <= number)
 Console.Write(current + " ");
 current += 2;
 }
+*/
