e1452d729fb3124cae34bf552d2066dc79c5869b
Pavel Maevsky <paha412011@mail.ru>
2023-03-07 23:07:45 +0300

    задача 2 закоменчена

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3 */
+a = -9 b = -3 -> max = -3 
 
 Console.Write("Input first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
@@ -25,4 +25,5 @@ else
         Console.WriteLine($"min is first number: {a}");
     }
 }
+*/
 
